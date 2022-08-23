78239994f8c6e0d6c6c6adefb847c270391ecbf1
Davis Davalos-DeLosh <git@tripd.anonaddy.com>
2022-08-22 19:27:58 -0600

    Reformatted and update

--- android2nix/mkBuild.nix
+++ android2nix/mkBuild.nix
@@ -2,30 +2,39 @@
 # It is designed for buidling `classical` Android apps.
 # It also can sign Apks for local used or debug with simple keystore
 # (usage of it is discouraged for release, see android2nix/keystore.nix).
-{ stdenv
-, pkgs
-, lib
-, config
-, androidComposition
-, android2nix
+{
+  stdenv,
+  pkgs,
+  lib,
+  config,
+  androidComposition,
+  android2nix,
   # User specified
-, src
-, pname
-, deps # deps.json path
-, jdk ? pkgs.jdk
-, gradlePkg ? pkgs.gradle
-, enableParallelBuilding ? true
-, buildType ? "assembleDebug"
-, nestedInAndroid ? false
-, extractApks ? true
-, autoSignApks ? true
-, keystore ? {}
-, ...
-}:
-let
-  inherit (lib)
-    toLower optionalString stringLength assertMsg
-    makeLibraryPath checkEnvVarSet foldl optional optionals
+  src,
+  pname,
+  deps, # deps.json path
+  jdk ? pkgs.jdk,
+  gradlePkg ? pkgs.gradle,
+  enableParallelBuilding ? true,
+  buildType ? "assembleDebug",
+  nestedInAndroid ? false,
+  extractApks ? true,
+  autoSignApks ? true,
+  keystore ? {},
+  preBuildHook ? "",
+  ...
+}: let
+  inherit
+    (lib)
+    toLower
+    optionalString
+    stringLength
+    assertMsg
+    makeLibraryPath
+    checkEnvVarSet
+    foldl
+    optional
+    optionals
     ;
 
   keystorePath = pkgs.callPackage ./keystore.nix {
@@ -34,13 +43,16 @@ let
     keyPassword = keystore.keyPassword or "nixPassword";
   };
 
-  latestBuildTools = foldl
-    (acc: tools: if builtins.compareVersions acc.version tools.version == -1 then tools else acc)
-    { version = ""; }
+  latestBuildTools =
+    foldl
+    (acc: tools:
+      if builtins.compareVersions acc.version tools.version == -1
+      then tools
+      else acc)
+    {version = "";}
     androidComposition.build-tools;
 
-  getBuildToolsBin = build-tools:
-    "${build-tools}/libexec/android-sdk/build-tools/${build-tools.version}";
+  getBuildToolsBin = build-tools: "${build-tools}/libexec/android-sdk/build-tools/${build-tools.version}";
 
   name = "${pname}-${buildType}-android";
 
@@ -48,79 +60,86 @@ let
     inherit deps androidComposition jdk gradlePkg enableParallelBuilding;
   };
 in
-stdenv.mkDerivation rec {
-  inherit name src;
+  stdenv.mkDerivation rec {
+    inherit name src;
 
-  nativeBuildInputs = with pkgs; [ bash jdk ]
-  ++ optionals stdenv.isDarwin [ file gnumake ];
+    nativeBuildInputs = with pkgs;
+      [bash jdk]
+      ++ optionals stdenv.isDarwin [file gnumake];
 
-  # Used by the Android Gradle build script in android/build.gradle
+    # Used by the Android Gradle build script in android/build.gradle
 
-  phases = [
-    "unpackPhase"
-  ]
-  ++ optional autoSignApks [ "keystorePhase" ]
-  ++ [
-    "buildPhase"
-    "installPhase"
-  ]
-  ++ optional autoSignApks [ "signPhase" ];
+    phases =
+      [
+        "unpackPhase"
+      ]
+      ++ optional autoSignApks ["keystorePhase"]
+      ++ [
+        "buildPhase"
+        "installPhase"
+      ]
+      ++ optional autoSignApks ["signPhase"];
 
-  unpackPhase = ''
-    cp -ar $src/. ./
-    chmod u+w -R ./
-    runHook postUnpack
-  '';
+    unpackPhase = ''
+      cp -ar $src/. ./
+      chmod u+w -R ./
+      runHook postUnpack
+    '';
 
-  postUnpack = ''
-    # Copy android/ directory
-    ${optionalString nestedInAndroid "cd android"}    
+    postUnpack = ''
+      # Copy android/ directory
+      ${optionalString nestedInAndroid "cd android"}
 
-    mkdir -p build
-    chmod -R +w .
+      mkdir -p build
+      chmod -R +w .
 
-    # Patch build.gradle and settings.gradle to use local repo
-    ${android2nix.patch-maven-source}
-  '';
+      # Patch build.gradle and settings.gradle to use local repo
+      ${android2nix.patch-maven-source}
+    '';
 
-  keystorePhase =
-    ''
+    keystorePhase = ''
       ${keystorePath.shellHook}
 
       export KEYSTORE_PATH="$PWD/${pname}.keystore"
       cp -a --no-preserve=ownership "${keystorePath}" "$KEYSTORE_PATH"
     '';
 
-  buildPhase =
-    ''
-      ${optionalString nestedInAndroid "cd android"}
+    __noChroot =
+      if pkgs.stdenv.isDarwin
+      then true
+      else false;
+
+    buildPhase = ''
+      ${preBuildHook}
+
+      # ${optionalString nestedInAndroid "cd android"}
 
       ${android2nixGradle}/bin/gradle ${buildType} || exit 1
     '';
 
-  installPhase = ''
-    mkdir -p $out
-    ${if extractApks
-  then
-    ''find . -name "*.apk" -exec cp {} $out/ \;''
-  else
-    "cp -r ./* $out/"}
-  '';
+    installPhase = ''
+      mkdir -p $out
+      ${
+        if extractApks
+        then ''find . -name "*.apk" -exec cp {} $out/ \;''
+        else "cp -r ./* $out/"
+      }
+    '';
 
-  signPhase = ''
-    cd $out
+    signPhase = ''
+      cd $out
 
-    for APK in $(find . -name "*.apk"); do
-        BASENAME=$(basename $APK .apk)
-        
-         ${getBuildToolsBin latestBuildTools}/apksigner sign \
-                                --verbose \
-                                --ks $KEYSTORE_PATH \
-                                --ks-pass pass:$KEYSTORE_PASSWORD \
-                                --key-pass pass:$KEYSTORE_KEY_PASSWORD \
-                                --ks-key-alias $KEYSTORE_ALIAS \
-                                --out "$BASENAME"-signed.apk \
-                                $APK
-    done
-  '';
-}
+      for APK in $(find . -name "*.apk"); do
+          BASENAME=$(basename $APK .apk)
+
+           ${getBuildToolsBin latestBuildTools}/apksigner sign \
+                                  --verbose \
+                                  --ks $KEYSTORE_PATH \
+                                  --ks-pass pass:$KEYSTORE_PASSWORD \
+                                  --key-pass pass:$KEYSTORE_KEY_PASSWORD \
+                                  --ks-key-alias $KEYSTORE_ALIAS \
+                                  --out "$BASENAME"-signed.apk \
+                                  $APK
+      done
+    '';
+  }
